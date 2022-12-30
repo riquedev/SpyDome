ba7f6e7596a656095eb128ad8c9623a17bd2fa0e
riquedev <rique_dev@hotmail.com>
2022-12-30 20:13:25 -0300

    :bug: fix circular import

--- DomeApp/dome/dome/pipelines/base.py
+++ DomeApp/dome/dome/pipelines/base.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from DomeApp.models import SpiderProcess
 
 
 class BaseSpyPipeline:
     __doc__ = ""
     __html__ = ""
     raw_item = None
-    def process_item(self, item, spider, last_pipeline: Optional[SpiderProcess]):
+
+    def process_item(self, item, spider, last_pipeline: Optional['SpiderProcess']):
         pass
